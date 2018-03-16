585516e0267411597fece27dcd71dc1b5ba58ce7
MarlenaFlueh <mf@esyoil.com>
2018-03-16 22:26:58 +0100

    add get route

--- server.js
+++ server.js
@@ -5,6 +5,17 @@ app.get('/', (req, res) => {
   res.send('hello world');
 });
 
+app.get('/person', (req, res) => {
+  res.send({
+    name: 'Liam',
+    hobbies: ['coding', 'cuddling'],
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    error: 'not found'
+  });
+});
+
 app.listen(3000);
-
-
