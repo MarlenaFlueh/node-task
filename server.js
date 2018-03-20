630a0484bdfd05bcf90acab32c35b27cb92bc039
MarlenaFlueh <mf@esyoil.com>
2018-03-20 22:25:40 +0100

    add partials

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
+const hbs = require('hbs');
 
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
