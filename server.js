aef7bbdbe048c34aa1dfc08065c4730bc312f648
MarlenaFlueh <mf@esyoil.com>
2018-03-21 23:38:25 +0100

    add first middleware

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const app = express();
 const hbs = require('hbs');
+const fs = require('fs');
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('toUpper', (text) => text.toUpperCase());
 
 hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
+
+app.use((req, res, next) => {
+  const now = new Date().toString();
+  const log = `${now}: ${req.method} ${req.url}`;
+  
+  fs.appendFile('server.log', log + '\n');
+  console.log(log);
+  
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
   res.render('home.hbs', {
