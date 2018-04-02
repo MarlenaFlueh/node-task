06372e1cc67b2395346b077d0eabecea76370cc6
MarlenaFlueh <mf@esyoil.com>
2018-04-02 21:12:57 +0200

    add projects.hbs

--- server.js
+++ server.js
@@ -31,20 +31,27 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'Home',
-    greeting: 'Welcome to this site!',
+    greeting: 'Welcome to this site!'
+  });
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Portfolio site',
+    greeting: 'Welcome to my portfolio'
   });
 });
 
 app.get('/person', (req, res) => {
   res.send({
     name: 'Liam',
-    hobbies: ['coding', 'cuddling'],
+    hobbies: ['coding', 'cuddling']
   });
 });
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
-    nameOfPerson: 'Sting',
+    nameOfPerson: 'Sting'
   });
 });
 
